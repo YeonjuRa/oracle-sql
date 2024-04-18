5a0209030a1aaa5f78be586e2cd75c7c3a6f2b55
GD <GD@DESKTOP-U8QQOUD>
2024-04-18 10:15:57 +0900

    dept, emp테이블용 VO 생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,7 @@
+package vo;
+//VO value Object > DTO(Data Transfer Object)>Domain ->컬럼 속성 값의 범위 (숫자만 가질수있다, 날짜만 가질 수 있다 등등) -> 왼쪽으로 범위가 커진다.
+public class Dept {
+	public int deptNo;
+	public String dName;
+	public String loc;
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,12 @@
+package vo;
+
+public class Emp {
+	public int empNo;
+	public String eName;
+	public String job;
+	public int mgr;
+	public String hireDate;
+	public int sal;
+	public int comm;
+	public int deptNo;
+}
